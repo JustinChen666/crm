ccc621748c485c74acdfcab90b44e5109c85ef69
ChenJun9401 <31572225+ChenJun9401@users.noreply.github.com>
2020-05-14 17:46:47 +0800

    使用生成器生成Employee和Department修改Mapper文件和对应类

---
+++ src/main/java/cn/wolfcode/crm/domain/Employee.java
@@ -0,0 +1,29 @@
+package cn.wolfcode.crm.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Setter
+@Getter
+public class Employee {
+    private Long id;
+
+    private String username;
+
+    private String realname;
+
+    private String password;
+
+    private String tel;
+
+    private String email;
+
+    private Department dept_id;
+
+    private Date hireDate;
+
+    private boolean state;
+
+    private Boolean admin;
+}
---
+++ src/main/java/cn/wolfcode/crm/mapper/DepartmentMapper.java
@@ -0,0 +1,16 @@
+package cn.wolfcode.crm.mapper;
+
+import cn.wolfcode.crm.domain.Department;
+import java.util.List;
+
+public interface DepartmentMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Department record);
+
+    Department selectByPrimaryKey(Long id);
+
+    List<Department> selectAll();
+
+    int updateByPrimaryKey(Department record);
+}
